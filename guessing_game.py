e32a949728bec0e6bad8646a361fdd57e0092838
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2020-09-06 16:57:21 +0700

    play again or exit

--- guessing_game.py
+++ guessing_game.py
@@ -1,8 +1,11 @@
 import random
 
-while True:
+game_status = True
+while game_status:
+    print("0")
     rand_num = random.randint(1,100)
-    while True:
+    print("1")
+    while game_status:
         try:
             num_in = input("Please guess a number between 1 and 100 : ")
             if num_in.isdigit():
@@ -11,10 +14,13 @@ while True:
                 raise Exception("Invalid Input! Try Again!")
             elif num_in < 1 or num_in > 100:
                 raise ValueError("Please guess a number within the given range")
-
+            print("2")
             # check answer
             if num_in == rand_num:
                 print("You got it")
+                AskPlayAgain = input("Type 'EXIT' to exit, or 'PLAY' to play again: ")
+                if AskPlayAgain == "EXIT":
+                    game_status = False
             elif num_in < rand_num:
                 print("It's lower")
             elif num_in > rand_num:
@@ -23,3 +29,4 @@ while True:
             print("Exception: ", err)
         except ValueError as err:
             print("ValueError: ", err)
+    
